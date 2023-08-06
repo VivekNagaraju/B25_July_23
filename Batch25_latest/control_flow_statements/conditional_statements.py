a929e6255d330fbb2da74eabf8581623c38bd1bb
admin <admin@192.168.1.42>
2023-08-06 16:46:26 +0530

    06-08-23

---
+++ Batch25_latest/control_flow_statements/conditional_statements.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number"))
+if a>0:
+    print("a is greater than zero")
+elif a<0:
+    print("a is lesser than zero")   
+else:
+    print("a is equal to zero")
+print("Program executed")
---
+++ Batch25_latest/control_flow_statements/for_loop.py
@@ -0,0 +1,9 @@
+# b=int(input("enter a number"))
+#
+# for a in range(b):
+#     print(a)
+
+my_list=[1, "abc", 3,  5.98, 5+4j ,6]
+
+for a in my_list:
+    print(a)
