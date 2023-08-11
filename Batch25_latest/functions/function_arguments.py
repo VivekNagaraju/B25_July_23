b1d26f79728e3dbbbf18b232ed7fe06862b1f273
admin <admin@DESKTOP-FDITP2P>
2023-08-11 11:34:55 +0530

    11-08-23

--- Batch25_latest/functions/function_arguments.py
+++ Batch25_latest/functions/function_arguments.py
@@ -2,17 +2,22 @@ def sum(a, b): # formal arguments. (Arguments are variables used to send input t
     c=a+b
     print(c)
     
-sum(2, 3) # actual arguments
+# sum(2, 3) # actual arguments
 
 def find_energy(m, c=6): # default arguments
     e=m*c**2
     print(e)
     
-find_energy(2)
-find_energy(3)
+# find_energy(2)
+# find_energy(3)
 
 def add(*a): # variable length arguments
     print(a)
     
-add(1, 2, 3 , 4, 5, 6 )
+# add(1, 2, 3 , 4, 5, 6 )
+
+def display(**a): # keyword variable length arguments
+    print(a)
+    
+# display(a1=2, b2=4, name="Vivek", object="pen", decimal=3.5)
 
---
+++ Batch25_latest/functions/import_functions.py
@@ -0,0 +1,10 @@
+from functions import function_arguments as fa
+from functions.function_arguments import *
+
+fa.find_energy(3)
+fa.sum(2, 3)
+find_energy(5)
+sum(2, 5)
+display(a=1)
+
+
