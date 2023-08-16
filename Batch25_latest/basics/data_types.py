a72380d488f93d8d4ead9009437012c1fbe24db6
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2023-08-16 11:49:21 +0530

    16-08-23: OOPs Introduction

--- Batch25_latest/basics/data_types.py
+++ Batch25_latest/basics/data_types.py
@@ -21,20 +21,20 @@ print(id(h))
 print(f, g, h)
 '''
 # from numbers import Complex
-'''
-from test.test_array import IntTest
-Re-intialisation
+
+
+# Re-intialisation
 a=0.4
 print(type(a))
 print(a)
 
 s=3+4j
 print(type(s))
-'''
-a=complex(input("please enter a number:"))
-print("value of a is", a)
-b=complex(input("enter the number:"))
-print("value of b is", b)
-c=a/b
-print(c)
+
+# a=complex(input("please enter a number:"))
+# print("value of a is", a)
+# b=complex(input("enter the number:"))
+# print("value of b is", b)
+# c=a/b
+# print(c)
 
---
+++ Batch25_latest/oops/oops_introduction.py
@@ -0,0 +1,15 @@
+'''
+
+'''
+#creating a class
+class Student:
+    def write_code(self):
+        print("I'm writing the code")
+
+vinayak=Student() #creating an object
+vinayak.write_code()
+print(type(vinayak))
+
+chetan=Student()
+chetan.write_code()
+print(type(chetan))
