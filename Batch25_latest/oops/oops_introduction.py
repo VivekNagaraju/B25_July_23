289168f2f061ba1af2d66bff1bdfa3d36237899f
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2023-08-17 11:24:21 +0530

    17-08-23: OOPs Day 2

--- Batch25_latest/oops/oops_introduction.py
+++ Batch25_latest/oops/oops_introduction.py
@@ -3,13 +3,30 @@
 '''
 #creating a class
 class Student:
-    def write_code(self):
-        print("I'm writing the code")
+    college="iQuest" # class/  static  variables
+    def __init__(self, name, roll_no):
+        self.name=name #instance/ object variables
+        self.roll_no=roll_no
+        print(f"This is {self.name} constructor")
+        
+    def write_code(self): #method variables
+        print(f"{self.name} writing the code")
+        
+    def display_details(self):
+        print(f"Name of the student is {self.name} and ID is {self.roll_no}")
 
-vinayak=Student() #creating an object
-vinayak.write_code()
-print(type(vinayak))
+vinayak=Student("Vinayak", 1) #creating an object
+# vinayak.write_code()
+# vinayak.display_details()
+# vinayak=Student("Vinayak2", 2)
+# vinayak.write_code
 
-chetan=Student()
-chetan.write_code()
-print(type(chetan))
+# print(type(vinayak))
+
+chetan=Student("Chethan", 2)
+# chetan.write_code()
+# vinayak.write_code()
+# chetan.write_code()
+# print(vinayak.name)
+# print(chetan.name)
+# print(type(chetan))
