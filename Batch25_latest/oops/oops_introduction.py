0687fbd53a7dda13bf8bd1e945b879eb4e0086f6
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2023-08-18 11:33:31 +0530

    18-08-23: OOPs- class/ static variables

--- Batch25_latest/oops/oops_introduction.py
+++ Batch25_latest/oops/oops_introduction.py
@@ -4,27 +4,36 @@
 #creating a class
 class Student:
     college="iQuest" # class/  static  variables
-    def __init__(self, name, roll_no):
+    def __init__(self, name):
         self.name=name #instance/ object variables
-        self.roll_no=roll_no
+        # self.roll_no=roll_no
+        # Student.college="iQuest" # declaring static variable inside the constructor
         print(f"This is {self.name} constructor")
-        
-    def write_code(self): #method variables
+    
+    
+    def write_code(self, roll_no): 
+        # roll_no=roll_no #method variables
+        self.roll_no=roll_no # instance variable inside a method
         print(f"{self.name} writing the code")
-        
+    
+       
     def display_details(self):
-        print(f"Name of the student is {self.name} and ID is {self.roll_no}")
+        # Student.college="iQuest"
+        print(f"Name of the student is {self.name}, ID is {self.roll_no} and college name is {Student.college}")
 
-vinayak=Student("Vinayak", 1) #creating an object
-# vinayak.write_code()
-# vinayak.display_details()
+vinayak=Student("Vinayak") #creating an object
+print(dir(vinayak))
+print(Student.__dict__)
+vinayak.write_code(101)
+vinayak.display_details()
 # vinayak=Student("Vinayak2", 2)
 # vinayak.write_code
 
 # print(type(vinayak))
 
-chetan=Student("Chethan", 2)
-# chetan.write_code()
+chetan=Student("Chethan")
+chetan.write_code(201)
+chetan.display_details()
 # vinayak.write_code()
 # chetan.write_code()
 # print(vinayak.name)
