56297c63a974d6491b4f1d9d7ffb89b35940f460
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2023-09-16 09:07:46 +0530

    16-09-23: Pagination completed

--- SeleniumTesting/basics/pagination_table.py
+++ SeleniumTesting/basics/pagination_table.py
@@ -9,11 +9,50 @@ driver.get("https://testautomationpractice.blogspot.com/")
 
 rows=driver.find_elements(By.XPATH, "//table[@id='productTable']/tbody/tr")
 rows_count=len(rows)
-for r in range(1, rows_count+1):
-    product_cell=driver.find_element(By.XPATH, f"//table[@id='productTable']/tbody/tr[{r}]/td[2]")
-    product_name=product_cell.text
-    print(product_name)
-    if product_name =="Product 4":
-        check_box=driver.find_element(By.XPATH, f"//table[@id='productTable']/tbody/tr[{r}]/td[4]/input")
-        check_box.click()
+
+'''
+# Pagination using nested For-Loop
+pages=driver.find_elements(By.CSS_SELECTOR, "#pagination > li")
+page_count=len(pages)
+
+for a in range(1, page_count+1):
+    next_page= driver.find_element(By.CSS_SELECTOR, f"#pagination > li:nth-child({a}) > a")
+    next_page.click()
+    clicked=False
+    for r in range(1, rows_count+1):
+        product_cell=driver.find_element(By.XPATH, f"//table[@id='productTable']/tbody/tr[{r}]/td[2]")
+        product_name=product_cell.text
+        print(product_name)
+        if product_name =="Product 12":
+            check_box=driver.find_element(By.XPATH, f"//table[@id='productTable']/tbody/tr[{r}]/td[4]/input")
+            check_box.click()
+            clicked=True
+            break
+    if clicked == True:
         break
+'''  
+
+# Pagination using nested While-Loop 
+while True:
+    clicked=False
+    for r in range(1, rows_count+1):
+        product_cell=driver.find_element(By.XPATH, f"//table[@id='productTable']/tbody/tr[{r}]/td[2]")
+        product_name=product_cell.text
+        print(product_name)
+        if product_name =="Product 12":
+            check_box=driver.find_element(By.XPATH, f"//table[@id='productTable']/tbody/tr[{r}]/td[4]/input")
+            check_box.click()
+            clicked=True
+            break
+        
+    if clicked == True:
+        break
+    next_page= driver.find_element(By.XPATH, "//a[@class='active']//parent::li//following-sibling::li[1]/a")
+    next_page.click()
+
+
+
+
+
+
+
